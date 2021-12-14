b3ab10e0705591ba7923339265079d68e6ea679b
Rahela Runcan <rahela.runcan@itiviti.com>
2021-12-14 16:58:14 +0200

    test
    Change-Id: I9012d1aaf2ede7332eefe4c9a05b2b6545273c7e

--- src/ProgramLogic.java
+++ src/ProgramLogic.java
@@ -6,22 +6,22 @@ public class ProgramLogic {
 
     public void runProgram() {
         boolean repeat;
-        boolean firstRepeat;
-        boolean secondRepeat;
+//        boolean firstRepeat;
+//        boolean secondRepeat;
         do {
             menu.printMenu();
             repeat = repeatProgram(read.getInt());
         } while (repeat);
 
-        do {
-            menu.printFirstSubMenu();
-            firstRepeat = firstSubMenu(read.getInt());
-        } while (firstRepeat);
-
-        do {
-            menu.printSecondSubMenu();
-            secondRepeat = secondSubMenu(read.getInt());
-        } while (secondRepeat);
+//        do {
+//            menu.printFirstSubMenu();
+//            firstRepeat = firstSubMenu(read.getInt());
+//        } while (firstRepeat);
+//
+//        do {
+//            menu.printSecondSubMenu();
+//            secondRepeat = secondSubMenu(read.getInt());
+//        } while (secondRepeat);
     }
 
     private boolean repeatProgram(int number) {
@@ -30,11 +30,13 @@ public class ProgramLogic {
                 System.out.println("Bye bye");
                 return false;
             case 1:
+                menu.printFirstSubMenu();
                 firstSubMenu(read.getInt());
                 return false;
             case 2:
+                menu.printSecondSubMenu();
                 secondSubMenu(read.getInt());
-                return true;
+                return false;
             default:
                 System.out.println(Messages.INCORECT_VALUE_MESSAGE);
                 return true;
@@ -45,17 +47,15 @@ public class ProgramLogic {
         switch (number) {
             case 0:
                 menu.printMenu();
+                repeatProgram(read.getInt());
                 return false;
             case 1:
-                menu.printFirstSubMenu();
                 options.doSum();
                 return true;
             case 2:
-                menu.printFirstSubMenu();
                 options.doSubstract();
                 return true;
             case 3:
-                menu.printFirstSubMenu();
                 options.doGetTemperatureCelsius();
                 return true;
             default:
@@ -68,49 +68,39 @@ public class ProgramLogic {
         switch (number) {
             case 0:
                 menu.printMenu();
+                repeatProgram(read.getInt());
                 return false;
             case 1:
-                menu.printSecondSubMenu();
                 options.doCheckNumberIntervalBetweenTwoAndEight();
                 return true;
             case 2:
-                menu.printSecondSubMenu();
                 options.doToFastTrackOrNotToFastTrack();
                 return true;
             case 3:
-                menu.printSecondSubMenu();
                 options.doPrintSwitchCase();
                 return true;
             case 4:
-                menu.printSecondSubMenu();
                 options.doisEligibleToVote();
                 return true;
             case 5:
-                menu.printSecondSubMenu();
                 options.doSumAndAverage();
                 return true;
             case 6:
-                menu.printSecondSubMenu();
                 options.doGetAverageFromInterval();
                 return true;
             case 7:
-                menu.printSecondSubMenu();
                 options.doComputeAverage();
                 return true;
             case 8:
-                menu.printSecondSubMenu();
                 options.doGetAverage();
                 return true;
             case 9:
-                menu.printSecondSubMenu();
                 options.doPopulateArray();
                 return true;
             case 10:
-                menu.printSecondSubMenu();
                 options.doPrintListFromNumber();
                 return true;
             case 11:
-                menu.printSecondSubMenu();
                 options.doReverseList();
                 return true;
             default:
